3e4b142edfeb2e89eb257d293d5ac504ee4c8dff
amorqw <134932135+amorqw@users.noreply.github.com>
2023-05-29 17:03:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
+import json
 from flask import Flask, render_template, request
 import random
 
 app = Flask(__name__)
 
-movies_db = [
-    {"title": "Фильм 1", "genre": "драма", "tags": ["тег1", "тег2", "тег3"]},
-    {"title": "Фильм 2", "genre": "комедия", "tags": ["тег2", "тег3"]},
-    {"title": "Фильм 3", "genre": "фантастика", "tags": ["тег1", "тег3"]},
-    {"title": "Фильм 4", "genre": "драма", "tags": ["тег1", "тег2"]},
-]
+# Загрузка данных из .json файла с использованием кодировки UTF-8
+with open('movies.json', 'r', encoding='utf-8') as json_file:
+    movies_db = json.load(json_file)
 
 @app.route('/')
 def index():
@@ -17,9 +15,21 @@ def index():
 @app.route('/random_movie', methods=['POST'])
 def random_movie():
     selected_genre = request.form.get('genre')
-    selected_tags = request.form.getlist('tags')
+    selected_duration = request.form.get('duration')
+    selected_novelty = request.form.get('novelty')
+    selected_age_limit = request.form.get('age_limit')
+    selected_country = request.form.get('country')
 
-    filtered_movies = [movie for movie in movies_db if movie['genre'] == selected_genre and all(tag in movie['tags'] for tag in selected_tags)]
+    filtered_movies = [movie for movie in movies_db if
+                       movie['genre'] == selected_genre and
+                       (selected_duration == 'short' and movie['duration'] < 120 or
+                        selected_duration == 'long' and movie['duration'] >= 120) and
+                       (selected_novelty == 'old' and movie['year'] < 2010 or
+                        selected_novelty == 'new' and movie['year'] >= 2010) and
+                       movie['age_limit'] == selected_age_limit and
+                       (selected_country == 'Россия' and movie['country'] == 'Россия' or
+                        selected_country == 'Не Россия' and movie['country'] != 'Россия')
+                       ]
 
     if filtered_movies:
         random_movie = random.choice(filtered_movies)
