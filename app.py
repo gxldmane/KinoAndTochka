bb9aa2ce12d9e8a6c16c268eb7153ff2f616ffb6
amorqw <134932135+amorqw@users.noreply.github.com>
2023-05-30 19:02:43 +0500

    Update app2.py

--- app.py
+++ app.py
@@ -6,16 +6,56 @@ from kinopoisk_unofficial.request.films.film_search_by_filters_request import Fi
 from kinopoisk_unofficial.request.films.film_request import FilmRequest
 
 app = Flask(__name__)
-api_client = KinopoiskApiClient("66cfec9c-4a01-47fd-9fc3-1caa79ecf41c")
+api_client = KinopoiskApiClient("47f5ff85-d969-4383-a8c5-59d3026b6696")
+
+genres_maps = {
+    "триллер": 1,
+    "драма": 2,
+    "криминал": 3,
+    "мелодрама": 4,
+    "детектив": 5,
+    "фантастика": 6,
+    "приключения": 7,
+    "биография": 8,
+    "фильм-нуар": 9,
+    "вестерн": 10,
+    "боевик": 11,
+    "фэнтези": 12,
+    "комедия": 13,
+    "военный": 14,
+    "история": 15,
+    "музыка": 16,
+    "ужасы": 17,
+    "мультфильм": 18,
+    "семейный": 19,
+    "мюзикл": 20,
+    "спорт": 21,
+    "документальный": 22,
+    "короткометражка": 23,
+    "аниме": 24,
+    "": 25,
+    "новости": 26,
+    "концерт": 27,
+    "для взрослых": 28,
+    "церемония": 29,
+    "реальное ТВ": 30,
+    "игра": 31,
+    "ток-шоу": 32,
+    "детский": 33
+}
 
 film_cache = {}  # Кэш для сохранения результатов запросов к API
 
-def get_filtered_films(genre):
-    if genre in film_cache:
-        filtered_list_of_films = film_cache[genre]
+def get_filtered_films(genre_id,rating_from,rating_to):
+    if genre_id in film_cache:
+        filtered_list_of_films = film_cache[genre_id]
     else:
         film_request = FilmSearchByFiltersRequest()
-        film_request.add_genre(FilterGenre(24, genre))
+        if film_request.genres:
+            film_request.genres = []
+        film_request.add_genre(FilterGenre(genre_id, ""))
+        film_request.rating_from = rating_from
+        film_request.rating_to = rating_to
 
         response = api_client.films.send_film_search_by_filters_request(film_request)
         result = response.items
@@ -26,7 +66,7 @@ def get_filtered_films(genre):
             film = get_film_details(i.kinopoisk_id)
             filtered_list_of_films.append(film)
 
-        film_cache[genre] = filtered_list_of_films
+        film_cache[genre_id] = filtered_list_of_films
 
     return filtered_list_of_films
 
@@ -46,24 +86,50 @@ def get_film_details(kinopoisk_id):
 def index():
     return render_template('index.html')
 
+
 @app.route('/random_movie', methods=['POST'])
 def random_movie():
     genre = request.form.get('genre')
+    is_series=request.form.get('is_series')
+    if is_series is None:
+        is_series='off'
+    ongoing = request.form.get('ongoing')
+    if ongoing is None:
+        ongoing='off'
+    rating_from = int(request.form.get('rating_from',0))
+    rating_to =int(request.form.get('rating_to', 10))
 
-    filtered_list_of_films = get_filtered_films(genre)
+    if genre in genres_maps:
+        genre_id = genres_maps[genre]
+        filtered_list_of_films = get_filtered_films(genre_id, rating_from, rating_to)
 
-    if filtered_list_of_films:
-        random_movie = random.choice(filtered_list_of_films)
-        name = random_movie.name_ru
-        length = random_movie.film_length
-        opisanie = random_movie.description
-        poster = random_movie.poster_url
-        serial = random_movie.serial
-        ongoing = random_movie.completed
 
-        return render_template('random_movie.html', movie=random_movie)
-    else:
-        return render_template('no_results.html')
+        if is_series == 'on':
+            filtered_list_of_films = [film for film in filtered_list_of_films if film.serial]
+            if ongoing == 'on':
+                filtered_list_of_films = [film for film in filtered_list_of_films if film.completed]
+            elif ongoing == 'off':
+                filtered_list_of_films = [film for film in filtered_list_of_films if not film.completed]
+        else:
+            if ongoing == 'on':
+                filtered_list_of_films = [film for film in filtered_list_of_films if not film.serial or film.completed]
+            elif ongoing == 'off':
+                filtered_list_of_films = [film for film in filtered_list_of_films if not film.serial or not film.completed]
+        filtered_list_of_films = [film for film in filtered_list_of_films if film.rating_kinopoisk and rating_from < float(film.rating_kinopoisk) < float(rating_to)]
+
+        if filtered_list_of_films:
+            random_movie = random.choice(filtered_list_of_films)
+            name = random_movie.name_ru
+            length = random_movie.film_length
+            opisanie = random_movie.description
+            poster = random_movie.poster_url
+            serial = random_movie.serial
+            ongoing = random_movie.completed
+
+            return render_template('random_movie.html', movie=random_movie)
+
+    return render_template('no_results.html')
+
 
 if __name__ == '__main__':
     app.run()
