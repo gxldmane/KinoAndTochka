d4f9753a463f408c3d817e8e75736503a7d618f9
Eme1yanovAA <126911605+Eme1yanovAA@users.noreply.github.com>
2023-06-21 17:28:53 +0500

    Update sctrips.js

--- static/sctrips.js
+++ static/sctrips.js
@@ -81,3 +81,16 @@ priceInput2.forEach(input =>{
            }
        });
    });
+
+const smoothLinks = document.querySelectorAll("a[href^='#']");
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener("click", function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute("href");
+
+        document.querySelector(id).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+};
